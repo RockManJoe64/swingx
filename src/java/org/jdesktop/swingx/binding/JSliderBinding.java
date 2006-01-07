a633657769759fa899cf9988e11fd8b94351608a
rbair <rbair@1312f61e-266d-0410-97fa-c3b71232c9ac>
2006-01-07 02:58:12 +0000

    Fixed NPE

--- src/java/org/jdesktop/swingx/binding/JSliderBinding.java
+++ src/java/org/jdesktop/swingx/binding/JSliderBinding.java
@@ -9,25 +9,33 @@
 
 package org.jdesktop.swingx.binding;
 import javax.swing.JSlider;
-import org.jdesktop.binding.FieldBinding;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
 
 /**
  *
  * @author Richard
  */
-public class JSliderBinding extends FieldBinding {
+public class JSliderBinding extends SwingColumnBinding {
     private int oldValue;
+    private boolean editable;
+    private boolean enabled;
+    private ValueListener listener;
+    private int cachedValue;
     
     public JSliderBinding(JSlider slider) {
-        super(slider, int.class);
+        super(slider, Integer.class);
+        listener = new ValueListener();
     }
 
-    protected void initialize() {
+    protected void doInitialize() {
         oldValue = getComponent().getValue();
+        getComponent().addChangeListener(listener);
     }
 
-    public void release() {
+    public void doRelease() {
         getComponent().setValue(oldValue);
+        getComponent().removeChangeListener(listener);
     }
     
     protected Object getComponentValue() {
@@ -35,10 +43,37 @@ public class JSliderBinding extends FieldBinding {
     }
 
     protected void setComponentValue(Object obj) {
+        listener.changing = true;
         getComponent().setValue((Integer)obj);
+        cachedValue = (Integer)obj;
+        listener.changing = false;
     }
     
     public JSlider getComponent() {
         return (JSlider)super.getComponent();
+    }
+
+    protected void setComponentEditable(boolean editable) {
+        this.editable = editable;
+        updateComponentEnabledState();
+    }
+    
+    protected void setComponentEnabled(boolean enabled) {
+        this.enabled = enabled;
+        updateComponentEnabledState();
+    }
+    
+    private void updateComponentEnabledState() {
+        getComponent().setEnabled(enabled && editable);
+    }
+    
+    private class ValueListener implements ChangeListener {
+        private boolean changing = false;
+        public void stateChanged(ChangeEvent e) {
+            if (!changing) {
+                setEdited(getComponent().getValue() != cachedValue);
+                save();
+            }
+        }
     }
 }
