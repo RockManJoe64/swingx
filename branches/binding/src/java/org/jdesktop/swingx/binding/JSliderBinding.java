2add42882de84d9365d43265fd6d3454d7f24a16
rbair <rbair@1312f61e-266d-0410-97fa-c3b71232c9ac>
2006-01-29 07:58:12 +0000

    changed the impl a bit, added comments

--- branches/binding/src/java/org/jdesktop/swingx/binding/JSliderBinding.java
+++ branches/binding/src/java/org/jdesktop/swingx/binding/JSliderBinding.java
@@ -13,46 +13,85 @@ import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
 
 /**
+ * A read/write binding for JSlider and subclasses of JSlider
  *
  * @author Richard
  */
 public class JSliderBinding extends SwingColumnBinding {
+    /**
+     * The original value (prior to binding)
+     */
     private int oldValue;
+    /**
+     * Whether or not the JSlider is editable. This, together with the
+     * enabled flag, indicate what state the JSlider should be in
+     * (JSlider only has one state by default, enabled)
+     */
     private boolean editable;
+    /**
+     * Whether or not the JSlider is enabled. This, together with the
+     * editable flag, indicate the state of the JSlider
+     */
     private boolean enabled;
+    /**
+     * Listens for changes in the value of the JSlider, and updates the
+     * edited state accordingly.
+     */
     private ValueListener listener;
-    private int cachedValue;
+    /**
+     * The value in the model -- used to calculate whether the state has changed
+     * for the JSlider
+     */
+    private int modelValue;
     
+    /**
+     */
     public JSliderBinding(JSlider slider) {
         super(slider, Integer.class);
         listener = new ValueListener();
     }
 
+    /**
+     * @inheritDoc
+     */
     protected void doInitialize() {
         oldValue = getComponent().getValue();
         getComponent().addChangeListener(listener);
     }
 
+    /**
+     * @inheritDoc
+     */
     public void doRelease() {
         getComponent().setValue(oldValue);
         getComponent().removeChangeListener(listener);
     }
     
+    /**
+     * @inheritDoc
+     */
     protected Object getComponentValue() {
         return getComponent().getValue();
     }
-
+    
+    /**
+     * @inheritDoc
+     */
     protected void setComponentValue(Object obj) {
-        listener.changing = true;
-        getComponent().setValue((Integer)obj);
-        cachedValue = (Integer)obj;
-        listener.changing = false;
+        modelValue = (Integer)obj;
+        listener.ignoreEvent = true;
+        getComponent().setValue(obj == null ? 0 : (Integer)obj);
+        listener.ignoreEvent = false;
     }
     
+    /**
+     * @inheritDoc
+     */
     public JSlider getComponent() {
         return (JSlider)super.getComponent();
     }
 
+    
     protected void setComponentEditable(boolean editable) {
         this.editable = editable;
         updateComponentEnabledState();
@@ -67,12 +106,16 @@ public class JSliderBinding extends SwingColumnBinding {
         getComponent().setEnabled(enabled && editable);
     }
     
+    /**
+     */
     private class ValueListener implements ChangeListener {
-        private boolean changing = false;
+        private boolean ignoreEvent = false;
         public void stateChanged(ChangeEvent e) {
-            if (!changing) {
-                setEdited(getComponent().getValue() != cachedValue);
-                save();
+            if (!ignoreEvent) {
+                setEdited(getComponent().getValue() != modelValue);
+                if (getAutoCommit() == AutoCommit.ON_CHANGE) {
+                    save();
+                }
             }
         }
     }
