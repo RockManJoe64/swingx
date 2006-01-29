36ad4a71c100de82f7e5966824b2d082f68e85fb
rbair <rbair@1312f61e-266d-0410-97fa-c3b71232c9ac>
2006-01-29 07:50:07 +0000

    added comments

--- branches/binding/src/java/org/jdesktop/swingx/binding/JProgressBarBinding.java
+++ branches/binding/src/java/org/jdesktop/swingx/binding/JProgressBarBinding.java
@@ -11,32 +11,51 @@ package org.jdesktop.swingx.binding;
 import javax.swing.JProgressBar;
 
 /**
+ * Binding implementation for JProgressBar and subclasses of JProgressBar.
+ * This binding is read-only.
  *
  * @author Richard
  */
 public class JProgressBarBinding extends SwingColumnBinding {
     private int oldValue;
     
+    /**
+     */
     public JProgressBarBinding(JProgressBar pb) {
         super(pb, int.class);
     }
 
+    /**
+     * @inheritDoc
+     */
     protected void doInitialize() {
         oldValue = getComponent().getValue();
     }
 
+    /**
+     * @inheritDoc
+     */
     public void doRelease() {
         getComponent().setValue(oldValue);
     }
     
-    protected Object getComponentValue() {
-        return getComponent().getValue();
-    }
-
+    /**
+     * @inheritDoc
+     */
     protected void setComponentValue(Object obj) {
         getComponent().setValue((Integer)obj);
     }
     
+    /**
+     * @inheritDoc
+     */
+    protected Object getComponentValue() {
+        throw new AssertionError("This method should never be called because the binding is readonly");
+    }
+
+    /**
+     * @inheritDoc
+     */
     public JProgressBar getComponent() {
         return (JProgressBar)super.getComponent();
     }
