d3547c6d5b14f725a91fcd43de05716850d42ce9
rbair <rbair@1312f61e-266d-0410-97fa-c3b71232c9ac>
2006-02-19 06:48:44 +0000

    added little flag to make it easier to NOT paint the divider

--- branches/binding/src/java/org/jdesktop/swingx/JXSplitPane.java
+++ branches/binding/src/java/org/jdesktop/swingx/JXSplitPane.java
@@ -9,16 +9,49 @@
 
 package org.jdesktop.swingx;
 
+import javax.swing.BorderFactory;
 import javax.swing.JSplitPane;
+import javax.swing.border.Border;
+import javax.swing.plaf.basic.BasicSplitPaneUI;
 
 /**
  *
  * @author Richard
  */
 public class JXSplitPane extends JSplitPane {
+    /**
+     * A flag specifying whether the divider should be painted. This is a
+     * hint which may be ignored by the look and feel
+     */
+    private boolean dividerPainted = true;
+    /**
+     * This is the divider Border, specified only if dividerPainted is true.
+     * I cache this border so I can restore it if the user toggles setDividerPainted
+     * back to true
+     */
+    private Border dividerBorder = null;
     
     /** Creates a new instance of JXSplitPane */
     public JXSplitPane() {
     }
     
+    public void setDividerPainted(boolean b) {
+        if (dividerPainted != b) {
+            // if possible, toggle whether a border is part of the divider
+            if (getUI() instanceof BasicSplitPaneUI) {
+                BasicSplitPaneUI ui = (BasicSplitPaneUI)getUI();
+                if (b) {
+                    dividerBorder = ui.getDivider().getBorder();
+                    ui.getDivider().setBorder(BorderFactory.createEmptyBorder());
+                } else {
+                    ui.getDivider().setBorder(dividerBorder);
+                }
+            }
+            firePropertyChange("dividerPainted", !b, b);
+        }
+    }
+    
+    public boolean isDividerPainted() {
+        return dividerPainted;
+    }
 }
