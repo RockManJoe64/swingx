bdcd170c7db7c0eb72f5e1207259632855be3058
joshy <joshy@1312f61e-266d-0410-97fa-c3b71232c9ac>
2007-02-27 02:00:18 +0000

    minor painting updates

--- branches/painter_work/src/java/org/jdesktop/swingx/JXGradientChooser.java
+++ branches/painter_work/src/java/org/jdesktop/swingx/JXGradientChooser.java
@@ -807,6 +807,37 @@ public class JXGradientChooser extends JXPanel {
                 LinearGradientPaint paint = new LinearGradientPaint(0f,0f,10f,0f,vals,colors);
                 MultipleGradientPaint grad = JXGradientChooser.showDialog(frame,"Pick a Gradient",paint);
                 System.out.println("got: " + JXGradientChooser.toString(grad));
+                //new LinearGradientPaint(pt1,pt2, new float[]{0f,1f}, new Color[]{ new Color(0,0,0,0), new Color(255,255,255,255)});
+                StringBuffer sb = new StringBuffer();
+                sb.append("new LinearGradientPaint(");
+                sb.append("new Point(0,0),");
+                sb.append("new Point(1,0),");
+                sb.append("new float[] {");
+                
+                for(int i=0; i<grad.getFractions().length; i++) {
+                    float f = grad.getFractions()[i];
+                    sb.append(f+"f");
+                    if(i < grad.getFractions().length-1) {
+                        sb.append(",");
+                    }
+                }
+                sb.append("},");
+                sb.append("new Color[] {");
+                for(int i=0; i<grad.getColors().length; i++) {
+                    Color c= grad.getColors()[i];
+                    sb.append("new Color(");
+                    sb.append(c.getRed()+","+c.getGreen()+","+c.getBlue()+","+c.getAlpha());
+                    sb.append(")");
+                    if(i < grad.getColors().length-1) {
+                        sb.append(",");
+                    }
+                }
+                sb.append("}");
+                sb.append(");");
+                p(sb.toString());
+            }
+            public void p(String s) {
+                System.out.println(s);
             }
         });
         frame.add(button);
