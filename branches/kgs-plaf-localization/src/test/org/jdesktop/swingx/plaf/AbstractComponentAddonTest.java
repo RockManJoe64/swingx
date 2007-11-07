6258b1cb8734f6436799b383d53ad635406bb6a8
nobody <nobody@1312f61e-266d-0410-97fa-c3b71232c9ac>
2007-11-07 12:37:58 +0000

    This commit was manufactured by cvs2svn to create branch 'kgs-plaf-localization'.

---
+++ branches/kgs-plaf-localization/src/test/org/jdesktop/swingx/plaf/AbstractComponentAddonTest.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package org.jdesktop.swingx.plaf;
+
+import junit.framework.TestCase;
+
+/**
+ * @author Karl George Schaefer
+ *
+ */
+public class AbstractComponentAddonTest extends TestCase {
+    /**
+     * Added until the centralization of the child testing works.
+     */
+    public void testDummy() {
+        
+    }
+}
