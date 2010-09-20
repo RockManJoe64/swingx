2ebaee97180174a6d8f0f4d56a628f2c4f140825
rah003 <rah003@1312f61e-266d-0410-97fa-c3b71232c9ac>
2010-09-20 13:38:35 +0000

    preparing 1.6.2 release

---
+++ tags/swingx-1.6.2/swingx-core/src/test/java/org/jdesktop/swingx/JXPanelVisualCheck.java
@@ -0,0 +1,147 @@
+/*
+ * $Id$
+ *
+ * Copyright 2006 Sun Microsystems, Inc., 4150 Network Circle,
+ * Santa Clara, California 95054, U.S.A. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ */
+package org.jdesktop.swingx;
+
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Paint;
+import java.awt.event.ActionEvent;
+
+import javax.imageio.ImageIO;
+import javax.swing.AbstractAction;
+import javax.swing.Action;
+import javax.swing.BorderFactory;
+import javax.swing.JComponent;
+import javax.swing.JPanel;
+
+import org.jdesktop.swingx.action.AbstractActionExt;
+import org.jdesktop.swingx.painter.ImagePainter;
+import org.jdesktop.swingx.painter.MattePainter;
+
+/**
+ * Contains methods to visually test JXPanel.
+ * 
+ * @author Jeanette Winzenburg
+ */
+public class JXPanelVisualCheck extends InteractiveTestCase {
+
+    public static void main(String args[]) {
+//      setSystemLF(true);
+      JXPanelVisualCheck test = new JXPanelVisualCheck();
+      try {
+         test.runInteractiveTests();
+//         test.runInteractiveTests(".*List.*");
+      } catch (Exception e) {
+          System.err.println("exception when executing interactive tests:");
+          e.printStackTrace();
+      }
+  }
+    
+    /**
+     * Issue #1199-swingx: JXPanel - must repaint on changes to background painter.
+     */
+    public void interactivePainterUpdate() {
+        JXPanel panel = new JXPanel();
+        final MattePainter painter = new MattePainter(Color.RED);
+        panel.setBackgroundPainter(painter);
+        JXFrame frame = wrapInFrame(panel, "background");
+        Action toggleColor = new AbstractAction("toggle background") {
+            
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Paint old = painter.getFillPaint();
+                painter.setFillPaint(old == Color.RED ? Color.YELLOW : Color.RED);
+                
+            }
+        };
+        addAction(frame, toggleColor);
+        show(frame, 200, 200);
+    }
+    /**
+     * Issue #1187-swingx: default scrollable tracks property prevents scrolling.
+     * Problem are the implementations of scrollableTracks: they unconditionally
+     * return true, so forcing the viewport/scrollpane to the size of the contained
+     * component.
+     */
+    public void interactiveScrolling() {
+        final JXPanel panel = new JXPanel(new BorderLayout());
+        panel.add(createScrollableContent(20));
+        JXFrame frame = wrapWithScrollingInFrame(panel, "scrollbar must be showing");
+        Action toggleHeightTrack = new AbstractActionExt("track height: " + panel.getScrollableHeightHint()) {
+            
+            ScrollableSizeHint[] tracks = new ScrollableSizeHint[] {
+                    ScrollableSizeHint.FIT
+                    , ScrollableSizeHint.NONE
+                    , ScrollableSizeHint.VERTICAL_STRETCH
+            };
+            int position;
+            
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                position++;
+                if (position >= tracks.length) position = 0;
+                panel.setScrollableHeightHint(tracks[position]);
+                setName("track height: " + tracks[position]);
+            }
+        }; 
+        addAction(frame, toggleHeightTrack);
+        show(frame, 400, 400);
+    }
+    /**
+     * @return
+     */
+    private JComponent createScrollableContent(int rows) {
+        JPanel component = new JPanel();
+        component.setPreferredSize(new Dimension(400, 400));
+        component.setMinimumSize(new Dimension(200, 200));
+        component.setMaximumSize(new Dimension(600, 600));
+        component.setBorder(BorderFactory.createLineBorder(Color.RED, 5));
+        
+//        JButton component = new JButton("gotcha!");
+//        JXTable table = new JXTable(rows, 6);
+//        for (int i = 0; i < table.getRowCount(); i++) {
+//            table.setValueAt("row: " + i, i, 0);
+//        }
+        return component;
+    }
+
+    public void interactiveIconPainter() throws Exception {
+        ImagePainter imagePainter = new ImagePainter(ImageIO.read(JXPanel.class.getResource("resources/images/kleopatra.jpg")));
+        JXPanel panel = new JXPanel();
+        panel.setBackgroundPainter(imagePainter);
+//        panel.setOpaque(false);
+        panel.setPreferredSize(new Dimension(200, 200));
+        showWithScrollingInFrame(panel, "icon painter in jxpanel");
+    }
+
+    /**
+     * do-nothing method - suppress warning if there are no other
+     * test fixtures to run.
+     *
+     */
+    public void testDummy() {
+        
+    }
+
+}
---
+++ tags/swingx-1.6.2/swingx-core/src/test/java/org/jdesktop/swingx/JXTaskPaneVisualCheck.java
@@ -0,0 +1,148 @@
+/*
+ * Copyright 2006 Sun Microsystems, Inc., 4150 Network Circle, Santa Clara,
+ * California 95054, U.S.A. All rights reserved.
+ * 
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ * 
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+ */
+package org.jdesktop.swingx;
+
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.KeyEvent;
+import java.util.Locale;
+import java.util.logging.Logger;
+
+import javax.swing.AbstractAction;
+import javax.swing.JButton;
+import javax.swing.JComponent;
+import javax.swing.JLabel;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JScrollPane;
+
+/**
+ * Simple tests to ensure that the {@code JXTaskPane} can be instantiated and
+ * displayed.
+ * 
+ * @author rah003
+ */
+public class JXTaskPaneVisualCheck extends InteractiveTestCase {
+    
+    @SuppressWarnings("unused")
+    private static final Logger LOG = Logger
+            .getLogger(JXTaskPaneVisualCheck.class.getName());
+    
+    public JXTaskPaneVisualCheck() {
+        super("JXLoginPane Test");
+    }
+
+    public static void main(String[] args) throws Exception {
+        JXTaskPaneVisualCheck test = new JXTaskPaneVisualCheck();
+        
+        try {
+            test.runInteractiveTests();
+//            test.runInteractiveTests("interactiveDisplay");
+//            test.runInteractiveTests("interactiveMnemonic");
+        } catch (Exception e) {
+            System.err.println("exception when executing interactive tests:");
+            e.printStackTrace();
+        }
+    }
+
+
+    public void interactiveMnemonic() {
+        JXTaskPane pane = new JXTaskPane();
+        pane.setTitle("Use Me");
+        pane.setMnemonic(KeyEvent.VK_U);
+        pane.setForeground(Color.RED);
+        pane.setBackground(Color.YELLOW);
+        pane.add(new JLabel("another"));
+        pane.add(new JButton("wow!!"));
+        JXTaskPaneContainer container = new JXTaskPaneContainer();
+        container.add(pane);
+        showInFrame(container, "Mnemonic Test", true);
+    }
+    
+    public void interactiveColors() {
+        JXTaskPane pane = new JXTaskPane();
+        pane.setTitle("just something....");
+        pane.setForeground(Color.RED);
+        pane.setBackground(Color.YELLOW);
+        pane.add(new JLabel("another"));
+        pane.add(new JButton("wow!!"));
+        JXTaskPaneContainer container = new JXTaskPaneContainer();
+        container.add(pane);
+        showInFrame(container, "background", true);
+    }
+    
+    /**
+     * Issue #249-swingx JXTaskPane looks ugly under different default LaFs
+     *
+     */
+    public void interactiveDisplay() {
+        // PENDING JW: removed while fixing #1186-swingx (no dependency on sun packages)
+        // revisit: why do we do this at all?
+//        sun.awt.AppContext.getAppContext().put("JComponent.defaultLocale", Locale.FRANCE);
+        JXTaskPane panel = new JXTaskPane();
+        panel.setTitle("Hi there");
+        panel.setForeground(Color.RED);
+        panel.setFont(new Font("tahoma",Font.BOLD, 72));
+        panel.add(new JLabel("Hi there again"));
+        showInFrame(panel, "JXTaskPane interactive", true);
+    }
+
+    /**
+     * Ensure that resizing scroll pane's properly enables/disables scrollbars.
+     * <p>
+     * SwingX issue #740
+     */
+    public void interactiveJScrollPaneTest() {
+        JXTaskPane panel = new JXTaskPane();
+        JXTable table = new JXTable(10, 15);
+        table.setHorizontalScrollEnabled(true);
+        panel.add(new JScrollPane(table));
+        showInFrame(panel, "Ensure scrolling works");
+    }
+
+    
+    @Override
+    protected void createAndAddMenus(JMenuBar menuBar, final JComponent component) {
+        super.createAndAddMenus(menuBar, component);
+        JMenu menu = new JMenu("Locales");
+        menu.add(new AbstractAction("Change Locale") {
+
+            public void actionPerformed(ActionEvent e) {
+                if (component.getLocale() == Locale.FRANCE) {
+                    component.setLocale(Locale.ENGLISH);
+                } else {
+                    component.setLocale(Locale.FRANCE);
+                }
+            }});
+        menuBar.add(menu);
+    }
+    
+    
+
+    /**
+     * Do nothing, make the test runner happy
+     * (would output a warning without a test fixture).
+     *
+     */
+    public void testDummy() {
+        
+    }
+
+}
